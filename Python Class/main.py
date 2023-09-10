11715fbde67fb573d1bbb893485e6b8fb675056a
levilu800b <levilu800b@gmail.com>
2023-09-10 14:09:27 +0100

    Adding Methods to class

--- Python Class/main.py
+++ Python Class/main.py
@@ -5,8 +5,19 @@ class User:
         self.followers = 0
         self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 user1 = User("001", "Angela")
 user2 = User("002", "Jack")
+
+user1.follow(user2)
+
+print(user1.followers)
+print(user1.following)
+print(user2.followers)
+print(user2.following)
 
 # user2 = User()
 # user2.id = "002"

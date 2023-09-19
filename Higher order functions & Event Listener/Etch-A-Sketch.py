c744efb47cc3a77d3be04651bad4e6cb0944f2d0
levilu800b <levilu800b@gmail.com>
2023-09-19 19:24:56 +0100

    etch a sketch

---
+++ Higher order functions & Event Listener/Etch-A-Sketch.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+screen = Screen()
+
+def move_forwards():
+    timmy.forward(10)
+
+def move_backwards():
+    timmy.backward(10)
+
+def move_left():
+    new_heading = timmy.heading() + 10
+    timmy.setheading(new_heading)
+
+def move_right():
+    new_heading = timmy.heading() - 10
+    timmy.setheading(new_heading)
+
+    def clear():
+        timmy.clear()
+        timmy.penup()
+        timmy.home()
+        timmy.pendown()
+
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.exitonclick()

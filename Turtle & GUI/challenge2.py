2c4f766cb3b79b65abc5711fe0276fb8cea01eb5
levilu800b <levilu800b@gmail.com>
2023-09-19 11:48:45 +0100

    GUI challenge 2

---
+++ Turtle & GUI/challenge2.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+for _ in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+screen = Screen()
+screen.exitonclick()

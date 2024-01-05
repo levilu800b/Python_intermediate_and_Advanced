82e338c65770f4aa57d6958e9cbacd6df9061c7f
levilu800b <levilu800b@gmail.com>
2024-01-05 17:02:01 +0000

    pandas series and new dataframe

--- Pandas & CSV/main.py
+++ Pandas & CSV/main.py
@@ -14,21 +14,45 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(data)
 # print(type(data["temp"])) # pandas.core.series.Series
 
-data_dict = data.to_dict()
-print(data_dict)
-
-temp_list = data["temp"].to_list()
-print(temp_list)
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
 
 # average = sum(temp_list) / len(temp_list)
 # print(average)
 
-average = data["temp"].mean()
-print(average)
+# average = data["temp"].mean()
+# print(average)
+#
+# max_temp = data["temp"].max()
+# print(max_temp)
 
-max_temp = data["temp"].max()
-print(max_temp)
+# Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+#
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9 / 5 + 32
+# print(monday_temp_F)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+

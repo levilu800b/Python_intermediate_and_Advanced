7998c64c5a60ce88ce079561c131a6b5559ee505
levilu800b <levilu800b@gmail.com>
2024-01-05 11:02:16 +0000

    write and create file

--- Open_Read_Write/main.py
+++ Open_Read_Write/main.py
@@ -8,3 +8,16 @@
 with open("my_file.txt") as file:
     contents = file.read()
     print(contents)
+
+# Write to a file
+
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# Append to a file
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+# Create a new file
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")

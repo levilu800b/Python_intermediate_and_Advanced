4c2486cf96b08a5eb9536aeb8d0fb33f20cfeba5
levilu800b <levilu800b@gmail.com>
2024-01-05 10:52:41 +0000

    open and read file

--- Open_Read_Write/main.py
+++ Open_Read_Write/main.py
@@ -1,4 +1,10 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# To read the file
+
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)

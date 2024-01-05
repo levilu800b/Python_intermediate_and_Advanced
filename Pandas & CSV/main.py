de00f64424cf81168cc61736811467a23aceaf5d
levilu800b <levilu800b@gmail.com>
2024-01-05 16:49:14 +0000

    pandas series

--- Pandas & CSV/main.py
+++ Pandas & CSV/main.py
@@ -15,4 +15,20 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data)
+# print(type(data["temp"])) # pandas.core.series.Series
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# average = sum(temp_list) / len(temp_list)
+# print(average)
+
+average = data["temp"].mean()
+print(average)
+
+max_temp = data["temp"].max()
+print(max_temp)

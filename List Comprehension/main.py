c7b624fdda358ec64ba43228bb27f801861982a7
levilu800b <levilu800b@gmail.com>
2024-01-07 19:20:17 +0000

    Conditional List comprehension

--- List Comprehension/main.py
+++ List Comprehension/main.py
@@ -1,12 +1,24 @@
+# For Loop
+
+numbers = [1, 2, 3, 4, 5, 6]
+doubled_numbers = []
+for num in numbers:
+    doubled_numbers.append(num * 2)
+print(doubled_numbers)
+
 # List Comprehension
 
 numbers = [1, 2, 3, 4, 5, 6]
 doubled_numbers = [num * 2 for num in numbers]
 print(doubled_numbers)
 
+# String as List
+
 name = 'Levi'
 letters_list = [letter for letter in name]
 print(letters_list)
+
+# Range as List
 
 range_list = [num * 2 for num in range(1, 5)]
 print(range_list)

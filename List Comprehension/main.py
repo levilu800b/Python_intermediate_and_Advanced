4fa958834a5e59fc93efa385e0c984af8e15647c
levilu800b <levilu800b@gmail.com>
2024-01-07 19:13:34 +0000

    List comprehension

---
+++ List Comprehension/main.py
@@ -0,0 +1,14 @@
+# List Comprehension
+
+numbers = [1, 2, 3, 4, 5, 6]
+doubled_numbers = [num * 2 for num in numbers]
+print(doubled_numbers)
+
+name = 'Levi'
+letters_list = [letter for letter in name]
+print(letters_list)
+
+range_list = [num * 2 for num in range(1, 5)]
+print(range_list)
+
+

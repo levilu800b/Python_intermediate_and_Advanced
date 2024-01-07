c3088deb7ffd3de62ee1f9efe489a9bdf76c6b2b
levilu800b <levilu800b@gmail.com>
2024-01-07 19:17:06 +0000

    Conditional List comprehension

--- List Comprehension/main.py
+++ List Comprehension/main.py
@@ -11,4 +11,11 @@ print(letters_list)
 range_list = [num * 2 for num in range(1, 5)]
 print(range_list)
 
+# Conditional List Comprehension
 
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)

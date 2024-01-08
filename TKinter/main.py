6b6391b1397794fe90cdef3deffc19e71d1a0b35
levilu800b <levilu800b@gmail.com>
2024-01-08 22:11:15 +0000

    Button

--- TKinter/main.py
+++ TKinter/main.py
@@ -1,12 +1,26 @@
-import tkinter
+from tkinter import *
 
 # Create a window
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="Button Got Clicked")
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
 
 window.mainloop()

1245f4db104e87a5a7586139904b1d9be93a4cdc
levilu800b <levilu800b@gmail.com>
2024-01-08 21:44:53 +0000

    Many keyword arguments

--- TKinter/playground.py
+++ TKinter/playground.py
@@ -19,3 +19,15 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)

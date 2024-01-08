7533c4f680e067b89db69fa946517ab17ec62c02
levilu800b <levilu800b@gmail.com>
2024-01-08 21:40:36 +0000

    **kwargs

--- TKinter/playground.py
+++ TKinter/playground.py
@@ -6,3 +6,16 @@ def add(*args):
 
 
 print(add(3, 5, 6, 7, 8, 9, 10))
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)

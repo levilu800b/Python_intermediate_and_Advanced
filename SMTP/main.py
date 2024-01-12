097ec23daf8dff3e4295f07673f513225b9e95fe
levilu800b <levilu800b@gmail.com>
2024-01-12 15:05:09 +0000

    quotes

--- SMTP/main.py
+++ SMTP/main.py
@@ -1,10 +1,31 @@
+# import smtplib
+#
+# my_email = "test@gmail.com"
+# password = "password"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="testdummy@yahoo.com", msg="Subject:Hello\n\nThis is the body of my email.")
+#     connection.close()
+
 import smtplib
+import datetime as dt
+import random
 
-my_email = "test@gmail.com"
-password = "password"
+MY_EMAIL = "test@gmail.com"
+MY_PASSWORD = "password"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="testdummy@yahoo.com", msg="Subject:Hello\n\nThis is the body of my email.")
-    connection.close()
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs="testdummy@gmail.com", msg=f"Subject:Monday Motivation\n\n{quote}")
+        connection.close()

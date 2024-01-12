726c4a5095ccd6407d376ce5627855a437f22357
levilu800b <levilu800b@gmail.com>
2024-01-12 14:28:04 +0000

    Datetime

---
+++ SMTP/date_time.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1997, month=8, day=24, hour=12)
+print(date_of_birth)

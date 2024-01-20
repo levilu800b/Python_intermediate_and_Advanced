9ada8c8ed2ec5991cfac74b74d617aae1b171311
levilu800b <levilu800b@gmail.com>
2024-01-20 15:13:56 +0000

    soup

--- Web Development Projects/bs4/main.py
+++ Web Development Projects/bs4/main.py
@@ -16,3 +16,12 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.li)
 # print(soup.li.string)
 # print(soup.find_all(name="li"))
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)

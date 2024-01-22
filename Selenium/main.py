a9bb6bff733b57e1f77272764ca9929acb5541ec
levilu800b <levilu800b@gmail.com>
2024-01-22 14:05:23 +0000

    selenium setup

---
+++ Selenium/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Keep Chrome browser open after program ends
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+

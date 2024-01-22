bea49b82f3682ade9cca97b0d5fa79951f81569c
levilu800b <levilu800b@gmail.com>
2024-01-22 11:48:00 +0000

    selecting particular elements using beautifulsoul

--- Web Development Projects/bs4/main.py
+++ Web Development Projects/bs4/main.py
@@ -25,3 +25,15 @@ for tag in all_anchor_tags:
 
 heading = soup.find(name="h1", id="name")
 print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select(".heading")
+print(headings)

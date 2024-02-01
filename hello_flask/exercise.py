8d0676a9645d2023ef1877cdc02ba1cc61498c3d
levilu800b <levilu800b@gmail.com>
2024-02-01 14:23:06 +0000

    exercise complete

--- hello_flask/exercise.py
+++ hello_flask/exercise.py
@@ -35,8 +35,14 @@ print(current_time)  # seconds since Jan 1st, 1970
 
 # Write your code below 👇
 
-def speed_calc_decorator():
-    pass
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+
+    return wrapper_function
 
 
 def fast_function():
@@ -47,3 +53,7 @@ def fast_function():
 def slow_function():
     for i in range(10000000):
         i * i
+
+
+fast_function()
+slow_function()
